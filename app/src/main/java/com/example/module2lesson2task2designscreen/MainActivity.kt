9cf40dcaa59dcc3cb0c4d3b547a8803ce00fa7ca
Ric <kbelyako@yandex.com>
2021-07-10 12:17:53 +0600

    init

---
+++ app/src/main/java/com/example/module2lesson2task2designscreen/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.module2lesson2task2designscreen
+/*
+ДЗ. Задание 2. Среднее
+Сверстайте скрин по дизайну.
+Иконки можно нарезать из принтскрина.
+ */
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
